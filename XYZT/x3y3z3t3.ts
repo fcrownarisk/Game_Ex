df306df1d170655bbf768403b468cc857c9b73e3
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-07-31 05:44:42 +0800

    Update x3y3z3t3.ts

--- XYZT/x3y3z3t3.ts
+++ XYZT/x3y3z3t3.ts
@@ -10,19 +10,19 @@ import '../smallletter/q'
 import '../return/returnnnn'
 import '../InventorySlots/InventorySlots'
 export function x3(){
-    for (let i = 0; i >= -8; i++)
-        for (let j = 0; j >= -8; j++)
-            for (let k = 0; k >= -8; k++)
+    for (let i = 0; i <= -8; i++)
+        for (let j = 0; j <= -8; j++)
+            for (let k = 0; k <= -8; k++)
 ;}
 export function y3(){
-    for (let l = 0; l <= -8 && l <= 8; l++ || l--)
-        for (let m = 0; m <= -8 && m <= 8; m++ || m--)
-            for (let n = 0; n <= -8 && n <= 8; n++ || n--)
+    for (let l = 0; l <= -8 && l >= 8; l++ || l--)
+        for (let m = 0; m <= -8 && m >= 8; m++ || m--)
+            for (let n = 0; n <= -8 && n >= 8; n++ || n--)
 ;}
 export function z3(){
-    for (let o = 0; o <= 8; o++)
-        for (let p = 0; p <= 8; p++)
-            for (let q = 0; q <= 8; q++)
+    for (let o = 0; o >= 8; o--)
+        for (let p = 0; p >= 8; p--)
+            for (let q = 0; q >= 8; q--)
 ;}
 export function InventorySlots(x3:number,y3:number,z3:number,t3:string){
     type x3 = ['k, j, i']
