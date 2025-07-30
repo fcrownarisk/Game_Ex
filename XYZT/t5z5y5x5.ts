8bcbe1855f6d715af403a25ec640b01fd8437245
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-07-31 05:57:15 +0800

    Update t5z5y5x5.ts

--- XYZT/t5z5y5x5.ts
+++ XYZT/t5z5y5x5.ts
@@ -11,27 +11,26 @@ import '../return/returnnnnnnn'
 import '../InventorySlots/InventorySlot567'
 export function x5(){
     for (let I = 0; I <= 4; ++I)
-       for (let J= 0;J <= 4; ++J)
+       for (let J= 0; J <= 4; ++J)
           for (let K= 0; K <= 4;++K)
-    console.log(I,J,K)
+    console.log(I, J, K)
 }
 export function y5() {
-    for (let L = 0; L <= -4 && L >= 4; ++L || L--)
-        for (let M = 0; M <= -4 && M >= 4; ++M || M--)
-            for (let N = 0; N <= -4 && N >= 4; ++N || N--)
-    console.log(L,M,N)
+    for (let L = 0; L >= -4 && L <= 4; L--||L++)
+        for (let M = 0; M >= -4 && M <= 4; M--||M++)
+            for (let N = 0; N >= -4 && N <= 4; N--||N++)
+    console.log(L, M, N)
 }
 export function z5(){
-    for (let O= 0; O <= -4;O--)
-       for (let P = 0; P <= -4; P--)
-          for (let Q = 0; Q <= -4;Q--)
-    console.log(O,P,Q)
+    for (let O= 0; O >= -4; O--)
+       for (let P = 0; P >= -4; P--)
+          for (let Q = 0; Q >= -4;Q--)
+    console.log(O, P, Q)
 }
-
 export function InventorySlot567(x5:number,y5:number,z5:number,t5:string){
     type x5 = ['Q,P,O']
     type y5 = ['N,M,L']
     type z5 = ['K,J,I']
-    type t5 = ['x5+y5+z5']
+    type t5 = ['x5*y5*z5']
 }
 console.log(InventorySlot567)
