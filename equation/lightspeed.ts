e07765c024862ecfb4f3898f17a2cf909f233792
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-07-31 05:38:38 +0800

    Create lightspeed.ts

---
+++ equation/lightspeed.ts
@@ -0,0 +1,7 @@
+const c = 299792458 m/s
+const PI = 3.141592653589793
+const e = 2.718281828459045
+//latex C C++ C#
+//typst RUST gleam bend
+var Sum = e 999999999999 math.pow(163,PI)
+var multiply = PI114514 e1919180
