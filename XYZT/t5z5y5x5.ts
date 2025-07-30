25bf824b5919d7611050efddf67f9a5c2c9c403b
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-07-31 05:48:11 +0800

    Update t5z5y5x5.ts

--- XYZT/t5z5y5x5.ts
+++ XYZT/t5z5y5x5.ts
@@ -16,15 +16,15 @@ export function x5(){
     console.log(I,J,K)
 }
 export function y5() {
-    for (let L = 0; L <= -4 && L <= 4; ++L || L--)
-        for (let M = 0; M <= -4 && M <= 4; ++M || M--)
-            for (let N = 0; N <= -4 && N <= 4; ++N || N--)
+    for (let L = 0; L <= -4 && L >= 4; ++L || L--)
+        for (let M = 0; M <= -4 && M >= 4; ++M || M--)
+            for (let N = 0; N <= -4 && N >= 4; ++N || N--)
     console.log(L,M,N)
 }
 export function z5(){
-    for (let O= 0; O <= 8;O++)
-       for (let P = 0; P <= 8; P++)
-          for (let Q = 0; Q <= 8;Q++)
+    for (let O= 0; O <= -4;O--)
+       for (let P = 0; P <= -4; P--)
+          for (let Q = 0; Q <= -4;Q--)
     console.log(O,P,Q)
 }
 
