9088a0c3f89b4ff119cf666cfd319279e4aafd18
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-10-28 09:52:57 +0800

    Update returnnnnnn.ts

--- return/returnnnnnn.ts
+++ return/returnnnnnn.ts
@@ -4,10 +4,10 @@ import '../smallletter/s'
 import '../smallletter/t'
 let t9:any,t10:any
 void function returnnnnnn(){
-    void  [   'q' & exp(0 + t9) + 'r'& log(NaN - t10),
-              's' & exp(0 - t9) + 't'& log(NaN + t10),
-              'q' & log(NaN + t9) + 'r'& exp(0 - t10),
-              's' & log(NaN - t9) + 't'& exp(0 - t10),
+    void  [   q & exp(0 + t9) + r & log(NaN - t10),
+              s & exp(0 - t9) + t & log(NaN + t10),
+              q & log(NaN + t9) + r & exp(0 - t10),
+              s & log(NaN - t9) + t & exp(0 - t10),
     ]
     return [t9,t10]
 }
