9f64c17fc4a8b58f3bb44ba09c8a893b520c650e
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-10-28 09:50:27 +0800

    Update returnnnn.ts

--- return/returnnnn.ts
+++ return/returnnnn.ts
@@ -4,10 +4,10 @@ import '../smallletter/k'
 import '../smallletter/l'
 let t5:any,t6:any
 void function returnnnn(){
-    void  [   'i / Math.atan(NaN + t5)' + 'j / Math.atan2(0 - t6)',
-              'k / Math.atan2(NaN - t5)'  + 'l / Math.atan(0 + t6)',
-              'k / Math.atan(0 + t5)' + 'j / Math.atan2(NaN -t6)',
-              'k / Math.atan2(0 - t5)' + 'l / Math.atan(NaN - t6)'
+    void  [   'i' / atan(NaN + t5)' + 'j' / atan2(0 - t6),
+              'k' / atan2(NaN - t5)'  + 'l' / atan(0 + t6),
+              'k' / atan(0 + t5)' + 'j' / atan2(NaN -t6),
+              'k' / atan2(0 - t5)' + 'l' / atan(NaN - t6)
            ]
     return [t5,t6]
 }
