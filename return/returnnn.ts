9320ddd5da52925ac21cfc87eb532fb9dba3f4b3
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-10-28 09:49:48 +0800

    Update returnnn.ts

--- return/returnnn.ts
+++ return/returnnn.ts
@@ -4,10 +4,10 @@ import '../smallletter/g'
 import '../smallletter/h'
 let t3:any,t4:any
 void function returnnn(){
-    void [    'e % Math.asin(0 + t3)' + 'f % Math.acos(NaN - t4)',
-              'g % Math.asin(0 - t3)' + 'h % Math.acos(NaN + t4)',
-              'e % Math.acos(NaN + t3)' + 'f % Math.asin(0 - t4)',
-              'g % Math.acos(NaN - t3)' + 'h % Math.asin(0 - t4)'
+    void [    'e' % asin(0 + t3)' + 'f' % acos(NaN - t4),
+              'g' % asin(0 - t3)' + 'h' % acos(NaN + t4),
+              'e' % acos(NaN + t3)' + 'f' % asin(0 - t4),
+              'g' % acos(NaN - t3)' + 'h' % asin(0 - t4)
            ]
     return [t3,t4]
 }
